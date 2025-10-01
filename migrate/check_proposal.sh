07cc0deef006783d3b9d611f20c5c3a4b7adc13d
Olga Kunyavskaya <olga.kunyavskaya@aurora.dev>
2025-10-01 16:35:54 +0100

    check proposal script

---
+++ migrate/check_proposal.sh
@@ -0,0 +1,28 @@
+PROPOSAL_ID=
+EXPECTED_NBTC_BS58_HASH=HgZnctwS7JdgbnXH6sbanQ9XoZCdf7iSz9vCdvMtRrGC
+DAO_ACCOUNT_ID=rainbowbridge.sputnik-dao.near
+
+PROP_JSON=actual_proposal.json
+WASM_PATH=decoded_args.wasm
+
+near view "$DAO_ACCOUNT_ID" get_proposal "{\"id\": $PROPOSAL_ID}" > $PROP_JSON
+
+if ! jq -e '.kind.FunctionCall.actions[0].args' "$PROP_JSON" >/dev/null 2>&1; then
+  echo "❌ kind.FunctionCall.actions[0].args not found"
+  echo "File: $PROP_JSON"
+  exit 1
+fi
+
+WASM_B64="$(jq -r '.kind.FunctionCall.actions[0].args' "$PROP_JSON")"
+printf '%s' "$WASM_B64" | base64 -d > "$WASM_PATH"
+
+DECODED_NBTC_BS58_HASH=$(sha256sum $WASM_PATH | awk '{print $1}' | xxd -r -p | base58)
+if [[ "$DECODED_NBTC_BS58_HASH" != "$EXPECTED_NBTC_BS58_HASH" ]]; then
+  echo "❌ Incorrect nBTC wasm hash"
+  echo "Expected: $EXPECTED_NBTC_BS58_HASH"
+  echo "Actual: $DECODED_NBTC_BS58_HASH"
+  exit 1
+else
+  echo "✅ nBTC wasm hash is correct"
+  exit 0
+fi
