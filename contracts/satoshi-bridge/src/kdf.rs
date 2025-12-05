11cd30aea49664d1385121cf9877697a8f1ef5b9
Olga Kunyavskaya <olga.kunyavskaya@aurora.dev>
2025-12-05 14:54:32 +0000

    reduce copypast

--- contracts/satoshi-bridge/src/kdf.rs
+++ contracts/satoshi-bridge/src/kdf.rs
@@ -5,18 +5,8 @@ use k256::elliptic_curve::sec1::ToEncodedPoint;
 
 #[near]
 impl Contract {
-    pub fn generate_public_key_pub(&self, path: String) -> String {
-        let mpc_pk = crypto_shared::near_public_key_to_affine_point(
-            self.internal_config()
-                .chain_signatures_root_public_key
-                .clone()
-                .expect("Missing chain_signatures_root_public_key"),
-        );
-        let epsilon = crypto_shared::derive_epsilon(&env::current_account_id(), &path);
-        let user_pk = crypto_shared::derive_key(mpc_pk, epsilon);
-        let user_pk_encoded_point = user_pk.to_encoded_point(false);
-
-        let public_key_bytes = user_pk_encoded_point.as_bytes().to_vec();
+    pub fn get_public_key_by_path(&self, path: String) -> String {
+        let public_key_bytes = self.generate_public_key(&path);
         let uncompressed_btc_public_key =
             BtcPublicKey::from_slice(&public_key_bytes).expect("Invalid public key bytes");
 
